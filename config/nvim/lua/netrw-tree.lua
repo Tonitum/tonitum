069ff06b29a579136a5444791ee90cea1ff057bc
Tonitum <cameron@tonitum.com>
2022-10-21 07:03:37 -0600

    Update netrw-tree.lua

--- config/nvim/lua/netrw-tree.lua
+++ config/nvim/lua/netrw-tree.lua
@@ -1,6 +1,7 @@
 -- to give the included fileexplorer a NERDTree like experience
 vim.g.netrw_banner = 0          -- hide top banner
 vim.g.netrw_liststyle = 3       -- tree view
+vim.g.netrw_keepdir = 0
 -- vim.g.netrw_browse_split = 0 -- no split
 -- vim.g.netrw_browse_split = 2 -- horizontal split
 vim.g.netrw_browse_split = 4    -- use previous window to open file
