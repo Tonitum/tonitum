a71d0eb694de452153d11023e7e91400386a6d8d
Tonitum <cameron@tonitum.com>
2022-10-21 06:59:52 -0600

    Update init.lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -6,6 +6,7 @@ require("netrw-tree") -- pseudo-nerdtree
 -- Key Mappings
 local map = require("map").map
 vim.g.mapleader = " " -- set leader key to space
+-- vim.g.mapleader = "," -- set leader key to space
 
 -- nmap("<leader><key>", "<cmd>Command<CR>")
 function nmap(shortcut, command)
@@ -36,17 +37,78 @@ nmap("K", "{")
 -- more convenient esc
 imap("jk", "<esc>")
 
+-- reload the config
+nmap("<leader>vs", "<cmd>so ~/.config/nvim/init.lua<CR>") -- re-source the config
+nmap("<leader>ve", "<cmd>e ~/.config/nvim/init.lua<CR>") -- edit the config
+
+-- tab navigation
+nmap("<leader>th", "<cmd>tabfirst<CR>")
+nmap("<leader>tk", "<cmd>tabnext<CR>")
+nmap("<leader>tj", "<cmd>tabprev<CR>")
+nmap("<leader>tl", "<cmd>tablast<CR>")
+nmap("<leader>tt", ":tabedit<Space>")
+nmap("<leader>tn", ":tabnew<Space>")
+nmap("<leader>tm", ":tabm<Space>")
+nmap("<leader>td", "<cmd>tabclose<CR>")
+
 -- show/hide explorer window
+nmap("<leader>Q", "<cmd>Lex %:p:h<CR>")
 nmap("<leader>E", "<cmd>Lex<CR>")
 
 -- buffer swapping
 nmap("<leader>bp", "<cmd>bprevious<CR>")
 nmap("<leader>bn", "<cmd>bnext<CR>")
 
+-- sort lines from visual selection
+map("x", "<leader>so", "<cmd>'<,'>!sort<CR>")
+
+-- replace all instances
+nmap("<leader><C-d>", ":%s//")
+
+-- bracket completion
+imap("{", "{}<left>")
+imap("{{", "{")
+imap("{}", "{}")
+
+imap("(", "()<left>")
+imap("((", "(")
+imap("()", "()")
+
+imap("[", "[]<left>")
+imap("[[", "[")
+imap("[]", "[]")
+
+imap('"', '""<left>')
+imap('""', '""')
+
+imap("'", "''<left>")
+imap("''", "''")
+
+-- apply suggested fix
+nmap("<leader>ap", "<cmd>:lua vim.lsp.buf.code_action()<CR>")
+-- vim-commentary
+-- map("x", "<C-/>", "<Plug>Commentary")
+-- nmap("<C-/>", "<Plug>Commentary")
+-- map("o", "<C-/>", "<Plug>Commentary")
+-- map("o", "<C-//>", "<Plug>Commentary")
+
 -- set Packer to auto reload
 vim.cmd [[
-    augroup Packer
-    autocmd!
-    autocmd BufWritePost init.lua PackerCompile
-    augroup end
+  augroup Packer
+  autocmd!
+  autocmd BufWritePost init.lua PackerCompile
+  augroup end
+]]
+
+-- tab types for c++ files
+vim.cmd [[
+  autocmd FileType cpp setlocal shiftwidth=2 tabstop=2
+]]
+
+vim.cmd [[
+  autocmd FileType lua setlocal shiftwidth=2 tabstop=2
+]]
+
+vim.cmd [[
+  autocmd FileType python setlocal shiftwidth=4 tabstop=4
 ]]
