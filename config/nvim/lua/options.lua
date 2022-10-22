bc4893d57d9a427bb9de430bed42d043242bbbe8
Cameron Davies <cameron@tonitum.com>
2022-10-22 07:10:13 -0600

    added work plugins config, removed duplicates from options, keeping colorscheme NeoVim

--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -79,82 +79,12 @@ for k, v in pairs(options) do
     vim.opt[k] = v
 end
 
--- visual
+-- colorscheme, etc 
 vim.cmd("colorscheme NeoSolarized")
 -- vim.cmd("colorscheme solarized")
 -- vim.cmd("colorscheme gruvbox")
 -- vim.cmd("hi Normal guibg=NONE ctermbg=NONE") -- transparency
 
--- path options
-
 vim.opt.shortmess:append("c") -- what do these do?
 vim.opt.isfname:append("@-@")
--- Manually set settings
--- [[
-    -- essential vim settings
-    -- -- tabs / indents
-    -- vim.opt.expandtab = true
-    -- vim.opt.shiftwidth = 4
-    -- vim.opt.shiftround = true
-    -- vim.opt.smartindent = true
-    -- vim.opt.softtabstop = 4
-    -- vim.opt.tabstop = 4
-    -- vim.o.smarttab = true
 
-    -- vim.opt.guicursor = ""
-    -- vim.opt.nu = true
-    -- vim.opt.relativenumber = true
-
-    -- vim.opt.errorbells = false
-    -- vim.opt.wrap = false
-    -- vim.wo.cursorline = true
-
-    -- -- swapfile/history
-    -- -- I don't really know what these do
-    --     -- vim.opt.swapfile = false
-    --     -- vim.opt.backup = false
-    --     -- vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-    --     -- vim.opt.undofile = true
-
-    -- -- search options
-    -- vim.opt.hlsearch = false
-    -- vim.opt.ignorecase = true
-    -- vim.opt.incsearch = true
-    -- vim.opt.smartcase = true
-
-    -- -- this breaks on the windows terminal
-    -- -- vim.opt.termguicolors = true
-
-    -- vim.opt.scrolloff = 8
-    -- vim.opt.signcolumn = "yes"
-    -- vim.opt.isfname:append("@-@")
-
-    -- -- Give more space for displaying messages.
-    -- vim.opt.cmdheight = 1
-
-    -- -- Having longer updatetime (default is 4000 ms = 4 s) leads to noticeable
-    -- -- delays and poor user experience.
-    -- vim.opt.updatetime = 50
-    -- vim.opt.timeoutlen = 1000
-    -- vim.opt.ttimeoutlen = 50
-
-    -- -- Don't pass messages to |ins-completion-menu|.
-    -- vim.opt.shortmess:append("c")
-
-    -- -- performance options
-    -- vim.o.complete = "i"
-    -- vim.o.lazyredraw = true
-
-    -- -- folding
-    -- vim.o.foldmethod = "indent"
-    -- vim.o.foldenable = false
-
-    -- -- performance
-    -- vim.o.autoread = true
-
-    -- -- colors
-    -- vim.cmd("colorscheme solarized")
-    -- vim.opt.colorcolumn = "80"
-    -- -- transparency
-    -- vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
-    -- ]]
