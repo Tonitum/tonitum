e54dfa4d4f579ebd094ee44be9b5af07274e7355
Tonitum <cameron@tonitum.com>
2022-11-28 07:16:09 -0700

    Update nerdtree.lua

--- config/nvim/lua/plugin-opt/nerdtree.lua
+++ config/nvim/lua/plugin-opt/nerdtree.lua
@@ -1,6 +1,6 @@
 -- Start NERDTree. If a file is specified, move the cursor to its window.
-vim.cmd( "autocmd StdinReadPre * let s:std_in=1 \
-autocmd VimEnter * NERDTree | if argc() > 0 || exists('s:std_in') | wincmd p | endif" )
+-- vim.cmd( "autocmd StdinReadPre * let s:std_in=1 \
+-- autocmd VimEnter * NERDTree | if argc() > 0 || exists('s:std_in') | wincmd p | endif" )
 -- Exit Vim if NERDTree is the only window remaining in the only tab.
 vim.cmd( "autocmd BufEnter * if tabpagenr('$') == 1 && winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | quit | endif" )
 
@@ -16,7 +16,7 @@ vim.cmd( "autocmd BufWinEnter * if getcmdwintype() == '' | silent NERDTreeMirror
 -- vim.cmd("autocmd VimEnter * NERDTree")
 
 -- Start NERDTree and put the cursor back in the other window.
--- vim.cmd( "autocmd VimEnter * NERDTree | wincmd p" )
+vim.cmd( "autocmd VimEnter * NERDTree | wincmd p" )
 
 -- Start NERDTree when Vim is started without file arguments.
 -- vim.cmd("autocmd StdinReadPre * let s:std_in=1 \
@@ -32,10 +32,9 @@ vim.cmd( "autocmd BufWinEnter * if getcmdwintype() == '' | silent NERDTreeMirror
 -- autocmd VimEnter * if argc() == 1 && isdirectory(argv()[0]) && !exists('s:std_in') |
 --     \ execute 'NERDTree' argv()[0] | wincmd p | enew | execute 'cd '.argv()[0] | endif
 
--- -- If another buffer tries to replace NERDTree, put it in the other window, and bring back NERDTree.
--- vim.cmd("autocmd BufEnter * if bufname('#') =~ 'NERD_tree_\d\+' && bufname('%') !~ 'NERD_tree_\d\+' && winnr('$') > 1 | \
--- let buf=bufnr() | buffer# | execute 'normal! \<C-W>w' | execute 'buffer'.buf | endif" )
+-- If another buffer tries to replace NERDTree, put it in the other window, and bring back NERDTree.
+-- vim.cmd
+ -- "autocmd BufEnter * if bufname('#') =~ 'NERD_tree_\\d\\+' &&  bufname('%') !~ 'NERD_tree_\\d\\+' && winnr('$') > 1 | let buf=bufnr() | buffer# | execute 'normal! \\<C-W>w' | execute 'buffer'.buf | endif"
 
 -- -- Mirror the NERDTree before showing it. This makes it the same on all tabs.
 -- nnoremap <C-n> :NERDTreeMirror<CR>:NERDTreeFocus<CR>
-
