f359a03bcda97504ee6685b0d5730a00b6a8aa45
Tonitum <cameron@tonitum.com>
2023-01-27 12:01:47 -0700

    Update init.lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,5 +1,6 @@
 -- Module Imports
 require("plugins") -- install plugins
+require("plugin-opt.telescope") -- install plugins
 require("options") -- essential vim settings
 require("keymap") -- alternate key maps
 local utils = require("utils") -- utility functions
@@ -12,6 +13,7 @@ local nmap = utils.nmap
 local imap = utils.imap
 local augroup = utils.augroup
 local autocmd = utils.autocmd
+local tonitum_group = augroup('Tonitum', {})
 
 -- show/hide explorer window
 if nerdtree ~= nil then
@@ -34,7 +36,7 @@ vim.cmd [[
 
 -- tab types for c++ files
 vim.cmd [[
-  autocmd FileType cpp setlocal shiftwidth=2 tabstop=2
+  autocmd FileType cpp setlocal shiftwidth=2 tabstop=2 commentstring=//\ %s
 ]]
 
 vim.cmd [[
@@ -48,9 +50,35 @@ vim.cmd [[
 vim.cmd [[
   autocmd FileType python setlocal shiftwidth=4 tabstop=4
 ]]
+
+vim.cmd [[
+  autocmd FileType markdown setlocal spell spelllang=en shiftwidth=2 tabstop=2
+]]
+
+autocmd('BufRead', {
+    group = tonitum_group,
+    pattern = '*.md, *.log, *.txt',
+    callback = function()
+      vim.g.wrap = true
+      vim.g.spelllang = 'en'
+      vim.g.spell = true
+    end,
+})
+
+vim.api.nvim_create_autocmd('BufReadPost', {
+  group = tonitum_group,
+  callback = function(args)
+    local valid_line = vim.fn.line([['"]]) >= 1 and vim.fn.line([['"]]) < vim.fn.line('$')
+    local not_commit = vim.b[args.buf].filetype ~= 'commit'
+
+    if valid_line and not_commit then
+      vim.cmd([[normal! g`"]])
+    end
+  end,
+})
+
 -- highlight on yank
 local yank_group = augroup('HighlightYank', {})
-
 autocmd('TextYankPost', {
     group = yank_group,
     pattern = '*',
