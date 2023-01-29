e3639013f29e192808fd6a72a010b1bb733cac38
Cameron Davies <cameron@tonitum.com>
2023-01-28 17:09:48 -0700

    added the keymaps I like

--- config/nvim/lua/tonitum/keymap.lua
+++ config/nvim/lua/tonitum/keymap.lua
@@ -1,7 +1,15 @@
 -- leader key to space
 vim.g.mapleader = " "
 
--- vim.keymap.set("n", "KEY", COMMAND)
+vim.keymap.set("n", "<leader>pv", "<cmd>Ex<CR>")
+-- move cursor to active window
+nmap("<leader>w", "<cmd>wincmd l<CR>")
+
+-- easier horizontal line navigation
+vim.keymap.set("n", "H", "^")
+vim.keymap.set("n", "L", "$")
+vim.keymap.set("v", "H", "^")
+vim.keymap.set("v", "L", "$")
 
 -- move highlighted line up/down with shift J/K
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -41,8 +49,8 @@ vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
--- ???
-vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
+-- find/replace all 
+-- vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
 
 -- add execute to file
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
