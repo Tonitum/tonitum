651fa94e0c7e9540f73a941211b1adda09e0d723
Cameron Davies <cameron@tonitum.com>
2023-03-05 14:52:35 -0700

    updated autocommands

--- config/nvim/lua/tonitum/autocommands.lua
+++ config/nvim/lua/tonitum/autocommands.lua
@@ -1,3 +1,19 @@
+-- highlight on yank
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
+
 -- tab types for c++ files
 vim.cmd [[
   autocmd FileType cpp setlocal shiftwidth=2 tabstop=2
