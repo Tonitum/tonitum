32de3839396b5b656e5bc384ea6867403c63ae13
Tonitum <cameron@tonitum.com>
2023-04-27 09:13:20 -0600

    Update airline.lua

--- config/nvim/after/plugin/airline.lua
+++ config/nvim/after/plugin/airline.lua
@@ -1,7 +1,9 @@
-vim.cmd('let g:airline_powerline_fonts = 1')
-vim.cmd('let g:airline_highlighting_cache = 1')
-vim.cmd('AirlineTheme everforest')
-vim.cmd [[
+vim.cmd[[  if !exists('g:airline_symbols')
+    let g:airline_symbols = {}
+  endif
+]]
+
+vim.cmd[[
   " powerline symbols
   let g:airline_left_sep = ''
   let g:airline_left_alt_sep = ''
@@ -12,5 +14,8 @@ vim.cmd [[
   let g:airline_symbols.readonly = ''
   let g:airline_symbols.linenr = ' :'
   let g:airline_symbols.maxlinenr = '☰ '
-  let g:airline_symbols.dirty='⚡'
 ]]
+
+vim.cmd('let g:airline_powerline_fonts = 1')
+vim.cmd('let g:airline_highlighting_cache = 1')
+vim.cmd('AirlineTheme everforest')
