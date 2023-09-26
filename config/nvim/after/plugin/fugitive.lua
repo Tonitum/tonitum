24bcaf9094b146a9515e4f59bcb5fd5189f6b267
Cameron <nighthunterthexiii@gmail.com>
2023-09-25 21:20:10 -0600

    added command to push

--- config/nvim/after/plugin/fugitive.lua
+++ config/nvim/after/plugin/fugitive.lua
@@ -7,6 +7,7 @@ vim.keymap.set("n", "<leader>gd", ":G diff<CR>") -- diff
 vim.keymap.set("n", "<leader>gB", ":G blame<CR>") -- blame
 vim.keymap.set("n", "<leader>ga", ":Gwrite<CR>") -- add the current file
 vim.keymap.set("n", "<leader>gc", ":G commit -m ''<left>") -- add the current file
+vim.keymap.set("n", "<leader>gPU", ":G push<CR>") -- add the current file
 
 local function openDiffSplit()
     vim.cmd("Gvdiffsplit")
