be3965e476556f98a74a42481c6c27cab4ac099d
Cameron Davies <cameron.davies@lmco.com>
2023-09-27 14:03:18 +0000

    adding a truecolor test script, added rosepine color stuff

--- config/nvim/after/plugin/colors.lua
+++ config/nvim/after/plugin/colors.lua
@@ -1,13 +1,19 @@
-if not pcall(vim.cmd, 'colorscheme everforest') then
-  print("colorscheme not loaded")
+local colorscheme = "rose-pine"
+if not pcall(vim.cmd, "colorscheme " ..colorscheme ) then
+  print(colorscheme .. " not loaded")
   return
 end
 
-vim.opt.termguicolors = true
-vim.cmd("set background=dark")
-vim.cmd("let g:everforest_disable_italic_comment = 1")
-vim.cmd("let g:everforest_background='hard'")
-vim.cmd("let g:everforest_ui_contrast='high'")
-vim.cmd("let g:everforest_diagnostic_virtual_text='colored'")
--- vim.cmd("let g:everforest_transparent_background = 1")
-vim.cmd('colorscheme everforest')
+if colorscheme == "everforest" then
+  vim.opt.termguicolors = true
+  vim.cmd("set background=dark")
+  vim.cmd("let g:everforest_disable_italic_comment = 1")
+  vim.cmd("let g:everforest_background='hard'")
+  vim.cmd("let g:everforest_ui_contrast='high'")
+  vim.cmd("let g:everforest_diagnostic_virtual_text='colored'")
+  -- vim.cmd("let g:everforest_transparent_background = 1")
+  vim.cmd("AirlineTheme everforest")
+elseif colorscheme == "rose-pine" then
+  vim.cmd("AirlineTheme luna")
+end
+vim.cmd("colorscheme " .. colorscheme)
