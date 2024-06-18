a06f9ac26061af4c1a12fd7a3914c60328ca891b
Tonitum <nighthunterthexiii@gmail.com>
2024-06-18 18:33:28 +0000

    feat: change back to everforest, formatting

--- config/nvim/lua/tonitum/plugins/colors.lua
+++ config/nvim/lua/tonitum/plugins/colors.lua
@@ -10,40 +10,27 @@ end
 
 return {
   -- color schemes
-  -- {"sainnhe/everforest",
-  --   dependencies = "vim-airline/vim-airline",
-  --   lazy = true,
-  --   config = function()
-  --     vim.cmd("let g:everforest_disable_italic_comment = 1")
-  --     vim.cmd("let g:everforest_background='hard'")
-  --     vim.cmd("let g:everforest_ui_contrast='high'")
-  --     vim.cmd("let g:everforest_diagnostic_virtual_text='colored'")
-  --     vim.cmd("let g:everforest_transparent_background = 1")
-  --     ColorMyPencils()
-  --   end
-  -- }, -- everforest color scheme
-  {
-  "neanias/everforest-nvim",
-  lazy = true,
-  priority = 1000, -- make sure to load this before all the other start plugins
-  -- Optional; default configuration will be used if setup isn't called.
-  config = function()
-    require("everforest").setup({
-      background = "hard",
-      ---How much of the background should be transparent. 2 will have more UI
-      ---components be transparent (e.g. status line background)
-      transparent_background_level = 0,
-      disable_italic_comments = true,
-    }
-    )
-    ColorMyPencils("everforest")
-    require("everforest").load()
-  end,
+  {"neanias/everforest-nvim",
+    lazy = true,
+    priority = 1000, -- make sure to load this before all the other start plugins
+    -- Optional; default configuration will be used if setup isn't called.
+    config = function()
+      require("everforest").setup({
+        background = "hard",
+        ---How much of the background should be transparent. 2 will have more UI
+        ---components be transparent (e.g. status line background)
+        transparent_background_level = 2,
+        disable_italic_comments = true,
+      }
+      )
+      ColorMyPencils("everforest")
+      require("everforest").load()
+    end,
   },
 
   {"rose-pine/neovim",
     name = "rose-pine",
-    lazy = false,
+    lazy = true,
     dependencies = "vim-airline/vim-airline",
     config = function ()
       require("rose-pine").setup({
@@ -52,8 +39,8 @@ return {
           transparency = true,
         },
       })
-      vim.cmd("AirlineTheme luna")
       ColorMyPencils("rose-pine")
+      vim.cmd("AirlineTheme luna")
     end
   }, -- rose pine color scheme
 
@@ -66,6 +53,78 @@ return {
       vim.g.nightflyTransparent = true
       vim.g.nightflyUnderlineMatchParen = true
     end
-  }
+  },
+
+  {'catppuccin/nvim',
+    lazy = true,
+    name = "catppuccin",
+    priority = 1000,
+    config = function ()
+      require("catppuccin").setup({
+        flavour = "auto", -- latte, frappe, macchiato, mocha
+        background = { -- :h background
+            light = "latte",
+            dark = "mocha",
+        },
+        transparent_background = false, -- disables setting the background color.
+        no_italic = true, -- Force no italic
+        integrations = {
+            cmp = true,
+            gitsigns = true,
+            nvimtree = true,
+            treesitter = true,
+            notify = false,
+            mini = {
+                enabled = true,
+                indentscope_color = "",
+            },
+            -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+        },
+    })
+    vim.cmd.colorscheme "catppuccin"
+    end
+  },
+
+  {"folke/tokyonight.nvim",
+    name = "tokyonight",
+    lazy = true,
+    priority = 1000, -- make sure to load this before all the other start plugins
+    -- Optional; default configuration will be used if setup isn't called.
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+      light_style = "day", -- The theme is used when the background is set to light
+      transparent = false, -- Enable this to disable setting the background color
+      terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
+      styles = {
+        -- Style to be applied to different syntax groups
+        -- Value is any valid attr-list value for `:help nvim_set_hl`
+        comments = { italic = false },
+        keywords = { italic = false },
+        functions = {},
+        variables = {},
+        -- Background styles. Can be "dark", "transparent" or "normal"
+        sidebars = "dark", -- style for sidebars, see below
+        floats = "dark", -- style for floating windows
+      },
+    }
+  },
+
+  {'maxmx03/solarized.nvim',
+    lazy = true,
+    priority = 1000,
+    config = function()
+      vim.o.background = 'dark' -- or 'light'
+      require('solarized').setup({
+        transparent = true, -- enable transparent background
+        styles = {
+          comments = { italic = false, bold = false },
+          functions = { italic = false },
+          variables = { italic = false },
+        }
+    })
+    end
+  },
 }
 
