0c1f80c2f61beec7e846d19a9d322f27cad263c8
Tonitum <cameron@tonitum.com>
2024-07-05 13:52:09 -0600

    feat: harpoon2 migration

--- config/nvim/lua/tonitum/plugins/harpoon.lua
+++ config/nvim/lua/tonitum/plugins/harpoon.lua
@@ -1,15 +1,17 @@
 return {
   "theprimeagen/harpoon",
-  config = function ()
-    local mark = require("harpoon.mark")
-    local ui = require("harpoon.ui")
+  branch = "harpoon2",
+  config = function()
+    local harpoon = require("harpoon")
+    harpoon:setup({})
+    vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 
-    vim.keymap.set("n", "<leader>a", mark.add_file)
-    vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
-    vim.keymap.set("n", "<C-j>", function() ui.nav_file(1) end)
-    vim.keymap.set("n", "<C-k>", function() ui.nav_file(2) end)
-    vim.keymap.set("n", "<C-l>", function() ui.nav_file(3) end)
-    vim.keymap.set("n", "<C-h>", function() ui.nav_file(4) end)
+    vim.keymap.set("n", "<C-j>", function() harpoon:list():select(1) end)
+    vim.keymap.set("n", "<C-k>", function() harpoon:list():select(2) end)
+    vim.keymap.set("n", "<C-l>", function() harpoon:list():select(3) end)
+    vim.keymap.set("n", "<C-h>", function() harpoon:list():select(4) end)
+
+    vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
   end
 }
