5888dde01177577c8d1a2f2637c85bfba4e06a8a
Tonitum <nighthunterthexiii@gmail.com>
2024-07-08 19:26:51 -0600

    patch: formatting and keeping netrw plugin

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,3 +1,1 @@
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
 require("tonitum")
--- config/nvim/lua/tonitum/plugins/ui.lua
+++ config/nvim/lua/tonitum/plugins/ui.lua
@@ -6,9 +6,7 @@ return {
         require("zen-mode").setup {
           window = {
             width = 120,
-            options = {
-
-            }
+            options = {}
           },
         }
         require("zen-mode").toggle()
