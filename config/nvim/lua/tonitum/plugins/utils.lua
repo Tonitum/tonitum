d285b4e00c8320becd5bd3c3cb16eb73aa824f76
Tonitum <nighthunterthexiii@gmail.com>
2025-10-20 10:38:55 -0600

    fix(nvim): fix the markdown-preview plugin

--- config/nvim/lua/tonitum/plugins/utils.lua
+++ config/nvim/lua/tonitum/plugins/utils.lua
@@ -49,17 +49,14 @@ return {
   {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = "markdown",
-    build = "cd app && yarn install",
+    build = "npx --yes yarn install",
     init = function()
       vim.g.mkdp_filetypes = { "markdown" }
     end,
+    ft = { "markdown" },
     config = function()
-      vim.g.mkdp_open_to_the_world = 1
-      vim.g.mkdp_port = 24603
       vim.g.mkdp_echo_preview_url = 1
-      vim.g.mkdp_open_ip = "127.0.0.1"
-      vim.g.mkdp_auto_start = 1
+      vim.g.mkdp_auto_start = 0
     end
   }
 }
