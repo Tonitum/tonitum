86079ef369023e7a2847ea56246d8729cd0af08a
Tonitum <cameron.davies@us.kbr.com>
2026-04-21 13:16:22 -0600

    deleted old config, created base modern config

---
+++ config/nvim/lua/tonitum/init.lua
@@ -0,0 +1,36 @@
+require("tonitum.options")
+require("tonitum.keymap")
+
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
+local TonitumGroup = augroup("Tonitum", {})
+local YankGroup = augroup("HighlightYank", {})
+
+-- what does this do?
+-- function R(name)
+--   require("plenary.reload").reload_module(name)
+-- end
+
+autocmd('TextYankPost', {
+  group = YankGroup,
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = 'IncSearch',
+      timeout = 40,
+    })
+  end,
+})
+
+-- what does this do?
+-- autocmd({"BufWritePre"}, {
+--   group = TonitumGroup,
+--   pattern = "*",
+--   command = "%s/\\s\\+$//e"
+-- })
+
+-- netrw settings
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
