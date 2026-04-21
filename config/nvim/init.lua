aa9bef1f5f5685dc6fb205c3eee7412443fc160a
Tonitum <nighthunterthexiii@gmail.com>
2026-04-21 13:21:00 -0600

    cleaning up init.lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -9,7 +9,7 @@ local opts = { noremap=true, silent=true }
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+vim.keymap.se:('n', '<space>q', vim.diagnostic.setloclist, opts)
 vim.g.mapleader = " "
 
 HOME = os.getenv("HOME")
