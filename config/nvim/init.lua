f068303931fcaa62cd220f02281f43340ae746ff
Tonitum <nighthunterthexiii@gmail.com>
2026-04-21 13:21:04 -0600

    added more nvim settings

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,4 +1,7 @@
 require('plugins')
+-- essential vim settings
+require("set")
+require("netrw-tree")
 
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
@@ -7,9 +10,10 @@ vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+vim.g.mapleader = " "
 
 HOME = os.getenv("HOME")
- -- set Packet to auto reload
+ -- set Packer to auto reload
 vim.cmd [[
 augroup Packer
   autocmd!
@@ -17,13 +21,3 @@ augroup Packer
 augroup end
 ]]
 
-
--- essential vim settings
-vim.wo.number = true
-vim.wo.relativenumber = true
-vim.wo.cursorline = true
-vim.o.hlsearch = false
--- colors
-vim.cmd("colorscheme solarized")
--- transparency
-vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
