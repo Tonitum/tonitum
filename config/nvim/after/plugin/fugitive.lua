b30c80d16364e55afaf816847cec16f813125cf5
Tonitum <cameron.davies@us.kbr.com>
2026-04-21 13:17:39 -0600

    git keybinds

--- config/nvim/after/plugin/fugitive.lua
+++ config/nvim/after/plugin/fugitive.lua
@@ -1,2 +1,5 @@
-vim.keymap.set("n", "<leader>gs", ":G status<CR>")
-vim.keymap.set("n", "<leader>gd", ":G diff<CR>")
+-- mercurial ftw
+vim.keymap.set("n", "<leader>hgs", ":G status<CR>") -- status
+vim.keymap.set("n", "<leader>hgd", ":G diff<CR>") -- diff
+vim.keymap.set("n", "<leader>hga", ":G add %<CR>") -- add the current file
+vim.keymap.set("n", "<leader>hgq", ":G difftool<CR>") -- add the current file
