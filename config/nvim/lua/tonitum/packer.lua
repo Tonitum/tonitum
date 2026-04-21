dab7c08bf61ea3b992793a981b59835e97b93606
Tonitum <cameron.davies@us.kbr.com>
2026-04-21 13:16:56 -0600

    finalizing plugin choices

--- config/nvim/lua/tonitum/packer.lua
+++ config/nvim/lua/tonitum/packer.lua
@@ -6,14 +6,13 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use {"wbthomason/packer.nvim", opt = true}
-  -- use( 'tpope/vim-fugitive' ) -- git commands in vim
-  use('airblade/vim-gitgutter')
-  use( 'tpope/vim-commentary' ) -- Use "gc" to comment lines in visual mode. Similarly to cmd+/ in other editors.
-  use( 'tpope/vim-surround' ) -- change text wrappers e.g. " ' ()
+  use('tpope/vim-fugitive') -- git commands in vim
+  use('tpope/vim-commentary') -- Use "gc" to comment lines in visual mode. Similarly to cmd+/ in other editors.
+  use('tpope/vim-surround') -- change text wrappers e.g. " ' ()
   use('vim-airline/vim-airline') -- fancy status bar
   use('preservim/nerdtree')
 
-  use {
+  use{
     'nvim-telescope/telescope.nvim', tag = '0.1.0',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
@@ -25,46 +24,38 @@ return require('packer').startup(function(use)
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use('nvim-treesitter/nvim-treesitter-textobjects') -- Additional textobjects for treesitter.
   use('nvim-treesitter/playground')
-  use( 'neovim/nvim-lspconfig' ) -- Collection of configurations for built-in LSP client.
-  use( 'hrsh7th/nvim-cmp' ) -- Autocompletion plugin
-  use( 'hrsh7th/cmp-nvim-lsp' ) -- LSP source for nvim-cmp
-  use( 'saadparwaiz1/cmp_luasnip' ) -- Snippets source for nvim-cmp
-  use({ 'L3MON4D3/LuaSnip',
-  commit ='94f192cf5f0a1931b3035c2d8affdebec460968c'}) -- Snippets plugin
 
-  use({'ellisonleao/gruvbox.nvim',
-  commit = 'cb7a8a867cfaa7f0e8ded57eb931da88635e7007',
-  config = function()
+  use{'ellisonleao/gruvbox.nvim',
+    commit = 'cb7a8a867cfaa7f0e8ded57eb931da88635e7007',
+    config = function()
       vim.cmd('colorscheme gruvbox')
     end
-  }) -- gruvbox color scheme
+  } -- gruvbox color scheme
 
-  use({'theprimeagen/harpoon',
+  use{'theprimeagen/harpoon',
     commit='6138ff7096bc50f0b6b40b607196232e1b90d6b6'
-  })
+  }
 
+  use{
+    'VonHeikemen/lsp-zero.nvim',
+    requires = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},
+      {'williamboman/mason.nvim'},
+      {'williamboman/mason-lspconfig.nvim'},
 
-  -- use {
-  -- 'VonHeikemen/lsp-zero.nvim',
-  -- requires = {
-  -- -- LSP Support
-  -- {'neovim/nvim-lspconfig'},
-  -- {'williamboman/mason.nvim'},
-  -- {'williamboman/mason-lspconfig.nvim'},
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-buffer'},
+      {'hrsh7th/cmp-path'},
+      {'saadparwaiz1/cmp_luasnip'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'hrsh7th/cmp-nvim-lua'},
 
-  -- -- Autocompletion
-  -- {'hrsh7th/nvim-cmp'},
-  -- {'hrsh7th/cmp-buffer'},
-  -- {'hrsh7th/cmp-path'},
-  -- {'saadparwaiz1/cmp_luasnip'},
-  -- {'hrsh7th/cmp-nvim-lsp'},
-  -- {'hrsh7th/cmp-nvim-lua'},
-
-  -- -- Snippets
-  -- {'L3MON4D3/LuaSnip'},
-  -- {'rafamadriz/friendly-snippets'},
-  -- }
-  -- }
-
-  -- use("folke/zen-mode.nvim")
+      -- Snippets
+      { 'L3MON4D3/LuaSnip',
+        commit ='94f192cf5f0a1931b3035c2d8affdebec460968c'},
+      {'rafamadriz/friendly-snippets'},
+    }
+  }
 end)
