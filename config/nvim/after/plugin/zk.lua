10d14efdb2273a0d036f89bd476272a925758a42
Tonitum <cameron.davies@us.kbr.com>
2026-04-21 13:18:01 -0600

    refs tonitum-20: updating zk keybinds

--- config/nvim/after/plugin/zk.lua
+++ config/nvim/after/plugin/zk.lua
@@ -1,22 +1,61 @@
+local notes_path = "~/zettelkasten/"
 require('telekasten').setup({
-  home = vim.fn.expand("~/zettelkasten"), -- Put the name of your notes directory here
-  dailies = vim.fn.expand("~/zettelkasten/daily"),
-  weeklies = vim.fn.expand("~/zettelkasten/weekly"),
-  templates = vim.fn.expand("~/zettelkasten/templates"),
+  home = vim.fn.expand(notes_path), -- Put the name of your notes directory here
+  dailies = vim.fn.expand(notes_path .. "/daily"),
+  weeklies = vim.fn.expand(notes_path .. "/weekly"),
+  templates = vim.fn.expand(notes_path .. "/templates"),
+  template_new_note = vim.fn.expand(notes_path .. "/templates/base.md"),
+  template_new_daily = vim.fn.expand(notes_path .. "/templates/daily.md"),
+  new_note_filename = 'title-uuid',
+  filename_space_subst = "_",
 })
 
+
+local tk = require('telekasten')
 -- Launch panel if nothing is typed after <leader>z
-vim.keymap.set("n", "<leader>z", "<cmd>Telekasten panel<CR>")
+vim.keymap.set("n", "<leader>z", function()
+    tk.panel()
+  end)
 
 -- Most used functions
-vim.keymap.set("n", "<leader>zf", "<cmd>Telekasten find_notes<CR>")
-vim.keymap.set("n", "<leader>zg", "<cmd>Telekasten search_notes<CR>")
-vim.keymap.set("n", "<leader>zd", "<cmd>Telekasten goto_today<CR>")
-vim.keymap.set("n", "<leader>zz", "<cmd>Telekasten follow_link<CR>")
-vim.keymap.set("n", "<leader>zn", "<cmd>Telekasten new_note<CR>")
-vim.keymap.set("n", "<leader>zc", "<cmd>Telekasten show_calendar<CR>")
-vim.keymap.set("n", "<leader>zb", "<cmd>Telekasten show_backlinks<CR>")
-vim.keymap.set("n", "<leader>zI", "<cmd>Telekasten insert_img_link<CR>")
+vim.keymap.set("n", "<leader>zf", function()
+    tk.find_notes()
+  end)
+vim.keymap.set("n", "<leader>zD", function()
+    tk.find_daily_notes()
+  end)
+vim.keymap.set("n", "<leader>zg", function()
+    tk.search_notes()
+  end)
+vim.keymap.set("n", "<leader>zd", function()
+    tk.goto_today()
+  end)
+vim.keymap.set("n", "<leader>zz", function()
+    tk.follow_link()
+  end)
+vim.keymap.set("n", "<leader>zn", function()
+    tk.new_note()
+  end)
+vim.keymap.set("n", "<leader>zN", function()
+    tk.new_templated_note()
+  end)
+vim.keymap.set("n", "<leader>zy", function()
+    tk.yank_notelink()
+  end)
+vim.keymap.set("n", "<leader>zc", function()
+    tk.show_calendar()
+  end)
+vim.keymap.set("n", "<leader>zt", function()
+    tk.toggle_todo()
+  end)
+vim.keymap.set("n", "<leader>zb", function()
+    tk.show_backlinks()
+  end)
+vim.keymap.set("n", "<leader>zI", function()
+    tk.insert_img_link()
+  end)
 
 -- Call insert link automatically when we start typing a link
-vim.keymap.set("i", "[[", "<cmd>Telekasten insert_link<CR>")
+vim.keymap.set("i", "[[", function()
+    tk.insert_link()
+  end)
