76da7d8442ad7c7d3da88e685db9ecb43fae5c3c
Tonitum <cameron.davies@us.kbr.com>
2026-04-21 13:17:24 -0600

    formatting

--- config/nvim/after/plugin/telescope.lua
+++ config/nvim/after/plugin/telescope.lua
@@ -5,7 +5,7 @@ vim.keymap.set('n', '<leader>fq', builtin.quickfix, {})
 vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fr', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fs', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+  builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
 
 vim.keymap.set('n', '<leader>gr', builtin.lsp_references)
