7501ca24d3d4c17b38537601db4a041f20865d97
Tonitum <cameron.davies@us.kbr.com>
2026-04-21 13:16:42 -0600

    formatting

--- config/nvim/lua/tonitum/packer.lua
+++ config/nvim/lua/tonitum/packer.lua
@@ -18,10 +18,10 @@ return require('packer').startup(function(use)
     requires = { {'nvim-lua/plenary.nvim'} }
   }
 
-  -- -- Highlight, edit, and navigate code using a fast incremental parsing library.
-  -- -- Treesitter is used by nvim for various things,
-  -- -- but among others, for syntax coloring.
-  -- -- Make sure that any themes you install support treesitter!
+  -- Highlight, edit, and navigate code using a fast incremental parsing library.
+  -- Treesitter is used by nvim for various things,
+  -- but among others, for syntax coloring.
+  -- Make sure that any themes you install support treesitter!
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use('nvim-treesitter/nvim-treesitter-textobjects') -- Additional textobjects for treesitter.
   use('nvim-treesitter/playground')
@@ -30,13 +30,14 @@ return require('packer').startup(function(use)
   use( 'hrsh7th/cmp-nvim-lsp' ) -- LSP source for nvim-cmp
   use( 'saadparwaiz1/cmp_luasnip' ) -- Snippets source for nvim-cmp
   use({ 'L3MON4D3/LuaSnip',
-  commit ='94f192cf5f0a1931b3035c2d8affdebec460968c'}) -- Snippets plugin
+      commit ='94f192cf5f0a1931b3035c2d8affdebec460968c'}) -- Snippets plugin
+
   use({'ellisonleao/gruvbox.nvim',
-  commit = 'cb7a8a867cfaa7f0e8ded57eb931da88635e7007',
-  config = function()
-    vim.cmd('colorscheme gruvbox')
-  end
-}) -- gruvbox color scheme
+      commit = 'cb7a8a867cfaa7f0e8ded57eb931da88635e7007',
+      config = function()
+        vim.cmd('colorscheme gruvbox')
+      end
+    }) -- gruvbox color scheme
 
 use('theprimeagen/harpoon')
 
