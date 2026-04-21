9d3dfd2718841296cb6dcea61684846b688f8fae
Tonitum <cameron.davies@us.kbr.com>
2026-04-21 13:18:14 -0600

    refs tonitum-20: added key to diff a split

--- config/nvim/after/plugin/fugitive.lua
+++ config/nvim/after/plugin/fugitive.lua
@@ -5,3 +5,10 @@ vim.keymap.set("n", "<leader>gb", ":G blame<CR>") -- blame
 vim.keymap.set("n", "<leader>ga", ":G add %<CR>") -- add the current file
 vim.keymap.set("n", "<leader>gc", ":G commit -m ''<left>") -- add the current file
 vim.keymap.set("n", "<leader>gq", ":G difftool<CR> :cclose<CR> :TroubleToggle quickfix<CR>") -- diff the current file
+
+local function openDiffSplit()
+    vim.cmd("Gvdiffsplit")
+    vim.keymap.set('n', 'q', "<c-w>c", { buffer = 0 })
+end
+vim.keymap.set("n", "<leader>gv", openDiffSplit) -- view the diff of the current file in a split
+
