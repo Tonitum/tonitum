9a022bd2c64c92c17471de4d3d913e77fb543414
Tonitum <cameron.davies@us.kbr.com>
2026-04-21 13:17:22 -0600

    telescope settings. bash aliases

--- config/nvim/after/plugin/telescope.lua
+++ config/nvim/after/plugin/telescope.lua
@@ -1,6 +1,31 @@
 local builtin = require('telescope.builtin')
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fq', builtin.quickfix, {})
 vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
+vim.keymap.set('n', '<leader>fr', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fs', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+
+vim.keymap.set('n', '<leader>gr', builtin.lsp_references)
+
+require('telescope').setup({
+  pickers = {
+    lsp_references = {
+      theme = "cursor"
+    },
+    find_files = {
+      theme = "ivy"
+    },
+    live_grep = {
+      theme = "ivy"
+    },
+    quickfix = {
+      theme = "ivy"
+    },
+    git_files = {
+      theme = "ivy"
+    }
+  }
+})
